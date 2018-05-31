437aff6d1dfdb31dd86c1fb4a272d8200561366f
cdeath <github@nunopereira.net>
2018-05-31 14:14:22 +0100

    bootstrap

---
+++ assets/js/main.js
@@ -0,0 +1,80 @@
+(function($, w, d, de, db) {
+
+    /* VARS */
+    de = d.documentElement;
+    db = d.body;
+
+    var breakpoints = {sm: 576, md: 768, lg: 992, xl: 1200},
+        api = 'https://c66a9fd4.ngrok.io/',
+        templates = {};
+
+    /* UTILS */
+    function throttle(cb, delay, debounce) {
+        delay = delay || 0;
+
+        var last = 0,
+            timeout;
+
+        function throttled() {
+            var now = +new Date(),
+                diff = debounce ? 0 : now - last,
+                args = arguments,
+                that = this;
+
+            if (diff >= delay) {
+                last = now;
+                return cb.apply(that, args);
+            } else {
+                timeout && clearTimeout(timeout);
+            }
+            timeout = setTimeout(function() {
+                timeout = null;
+                return cb.apply(that, args);
+            }, delay - diff);
+        }
+
+        return throttled;
+    }
+
+    function toggleLoading(evt) {
+        $(de).toggleClass('loading', evt.type === 'ajaxStart');
+    }
+
+    /* METHODS */
+    function getLast() {
+        $.get(api + '/last', function(res) {
+            console.log(res);
+            //$('#last').html(Mustache.render(templates['template-last'], res));
+        });
+    }
+
+    function getPlayers() {
+        $.get(api + '/players', function(res) {
+            console.log(res);
+            //$('#players').html(Mustache.render(templates['template-players'], res));
+        });
+    }
+
+    function getPlayer(player) {
+        $.get(api + '/players/' + player, function(res) {
+            console.log(res);
+            //$('#player').html(Mustache.render(templates['template-player'], res));
+        });
+    }
+
+
+    function getStuff() {
+        getLast();
+    }
+
+    /* EVENT LISTENERS */
+    $(d).on('ajaxStart ajaxStop', toggleLoading);
+
+    /* INIT */
+    $('script[type="x-tmpl-mustache"]').each(function(idx, elm) {
+        templates[elm.id] = $(elm).html();
+        Mustache.parse(templates[elm.id]);
+    });
+    !$(de).is('.not-found') && getStuff();
+
+}(jQuery, window, document));
